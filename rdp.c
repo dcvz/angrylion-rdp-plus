68d9cd70ed66fa28b77891179149a4287f3573db
ata4 <barracuda415@yahoo.de>
2017-04-22 17:01:55 +0200

    Moved private header stuff to rdp.c

--- rdp.c
+++ rdp.c
@@ -1,11 +1,121 @@
-
 #include "rdp.h"
 #include "gfx_1.3.h"
-#include "tctables.h"
 #include "msg.h"
 #include "screen.h"
 
 #include <stdio.h>
+
+// z64.h
+#define SP_INTERRUPT	0x1
+#define SI_INTERRUPT	0x2
+#define AI_INTERRUPT	0x4
+#define VI_INTERRUPT	0x8
+#define PI_INTERRUPT	0x10
+#define DP_INTERRUPT	0x20
+
+#define SP_STATUS_HALT			0x0001
+#define SP_STATUS_BROKE			0x0002
+#define SP_STATUS_DMABUSY		0x0004
+#define SP_STATUS_DMAFULL		0x0008
+#define SP_STATUS_IOFULL		0x0010
+#define SP_STATUS_SSTEP			0x0020
+#define SP_STATUS_INTR_BREAK	0x0040
+#define SP_STATUS_SIGNAL0		0x0080
+#define SP_STATUS_SIGNAL1		0x0100
+#define SP_STATUS_SIGNAL2		0x0200
+#define SP_STATUS_SIGNAL3		0x0400
+#define SP_STATUS_SIGNAL4		0x0800
+#define SP_STATUS_SIGNAL5		0x1000
+#define SP_STATUS_SIGNAL6		0x2000
+#define SP_STATUS_SIGNAL7		0x4000
+
+#define DP_STATUS_XBUS_DMA		0x01
+#define DP_STATUS_FREEZE		0x02
+#define DP_STATUS_FLUSH			0x04
+#define DP_STATUS_START_GCLK		0x008
+#define DP_STATUS_TMEM_BUSY		0x010
+#define DP_STATUS_PIPE_BUSY		0x020
+#define DP_STATUS_CMD_BUSY			0x040
+#define DP_STATUS_CBUF_READY		0x080
+#define DP_STATUS_DMA_BUSY			0x100
+#define DP_STATUS_END_VALID		0x200
+#define DP_STATUS_START_VALID		0x400
+
+
+#define LSB_FIRST 1 
+#ifdef LSB_FIRST
+	#define BYTE_ADDR_XOR		3
+	#define WORD_ADDR_XOR		1
+	#define BYTE4_XOR_BE(a) 	((a) ^ 3)				
+#else
+	#define BYTE_ADDR_XOR		0
+	#define WORD_ADDR_XOR		0
+	#define BYTE4_XOR_BE(a) 	(a)
+#endif
+
+#ifdef LSB_FIRST
+#define BYTE_XOR_DWORD_SWAP 7
+#define WORD_XOR_DWORD_SWAP 3
+#else
+#define BYTE_XOR_DWORD_SWAP 4
+#define WORD_XOR_DWORD_SWAP 2
+#endif
+#define DWORD_XOR_DWORD_SWAP 1
+
+#define INLINE
+#ifdef _MSC_VER
+#define STRICTINLINE	__forceinline
+#else
+#define STRICTINLINE	inline
+#endif
+
+#define rdram ((uint32_t*)gfx.RDRAM)
+#define rsp_imem ((uint32_t*)gfx.IMEM)
+#define rsp_dmem ((uint32_t*)gfx.DMEM)
+
+#define rdram16 ((uint16_t*)gfx.RDRAM)
+#define rdram8 (gfx.RDRAM)
+
+#define vi_origin (*(uint32_t*)gfx.VI_ORIGIN_REG)
+#define vi_width (*(uint32_t*)gfx.VI_WIDTH_REG)
+#define vi_control (*(uint32_t*)gfx.VI_STATUS_REG)
+#define vi_v_sync (*(uint32_t*)gfx.VI_V_SYNC_REG)
+#define vi_h_sync (*(uint32_t*)gfx.VI_H_SYNC_REG)
+#define vi_h_start (*(uint32_t*)gfx.VI_H_START_REG)
+#define vi_v_start (*(uint32_t*)gfx.VI_V_START_REG)
+#define vi_v_intr (*(uint32_t*)gfx.VI_INTR_REG)
+#define vi_x_scale (*(uint32_t*)gfx.VI_X_SCALE_REG)
+#define vi_y_scale (*(uint32_t*)gfx.VI_Y_SCALE_REG)
+#define vi_timing (*(uint32_t*)gfx.VI_TIMING_REG)
+#define vi_v_current_line (*(uint32_t*)gfx.VI_V_CURRENT_LINE_REG)
+
+#define dp_start (*(uint32_t*)gfx.DPC_START_REG)
+#define dp_end (*(uint32_t*)gfx.DPC_END_REG)
+#define dp_current (*(uint32_t*)gfx.DPC_CURRENT_REG)
+#define dp_status (*(uint32_t*)gfx.DPC_STATUS_REG)
+
+// tctables.h
+const int32_t norm_point_table[64] = {
+    0x4000, 0x3f04, 0x3e10, 0x3d22, 0x3c3c, 0x3b5d, 0x3a83, 0x39b1,
+    0x38e4, 0x381c, 0x375a, 0x369d, 0x35e5, 0x3532, 0x3483, 0x33d9,
+    0x3333, 0x3291, 0x31f4, 0x3159, 0x30c3, 0x3030, 0x2fa1, 0x2f15,
+    0x2e8c, 0x2e06, 0x2d83, 0x2d03, 0x2c86, 0x2c0b, 0x2b93, 0x2b1e,
+    0x2aab, 0x2a3a, 0x29cc, 0x2960, 0x28f6, 0x288e, 0x2828, 0x27c4,
+    0x2762, 0x2702, 0x26a4, 0x2648, 0x25ed, 0x2594, 0x253d, 0x24e7,
+    0x2492, 0x243f, 0x23ee, 0x239e, 0x234f, 0x2302, 0x22b6, 0x226c,
+    0x2222, 0x21da, 0x2193, 0x214d, 0x2108, 0x20c5, 0x2082, 0x2041 
+};
+
+const int32_t norm_slope_table[64] = {
+    0xf03, 0xf0b, 0xf11, 0xf19, 0xf20, 0xf25, 0xf2d, 0xf32,
+    0xf37, 0xf3d, 0xf42, 0xf47, 0xf4c, 0xf50, 0xf55, 0xf59,
+    0xf5d, 0xf62, 0xf64, 0xf69, 0xf6c, 0xf70, 0xf73, 0xf76,
+    0xf79, 0xf7c, 0xf7f, 0xf82, 0xf84, 0xf87, 0xf8a, 0xf8c,
+    0xf8e, 0xf91, 0xf93, 0xf95, 0xf97, 0xf99, 0xf9b, 0xf9d,
+    0xf9f, 0xfa1, 0xfa3, 0xfa4, 0xfa6, 0xfa8, 0xfa9, 0xfaa,
+    0xfac, 0xfae, 0xfaf, 0xfb0, 0xfb2, 0xfb3, 0xfb5, 0xfb5,
+    0xfb7, 0xfb8, 0xfb9, 0xfba, 0xfbc, 0xfbc, 0xfbe, 0xfbe
+};
 
 extern GFX_INFO gfx;
 
