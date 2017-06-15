a11e97d837b6bf971f0d4ef69cc1f4492e3d62c3
ata4 <barracuda415@yahoo.de>
2017-06-15 14:19:03 +0200

    Don't know anymore why I changed the vactivelines calculation, but it proved to be wrong for some games, so it's time to revert it

--- core/vi.c
+++ core/vi.c
@@ -950,8 +950,7 @@ int vi_process_init(void)
         msg_error("VI_V_SYNC_REG too big");
     if (vactivelines < 0)
         return 0;
-    //vactivelines >>= lineshifter;
-    vactivelines = (ispal ? 576 : 480) >> lineshifter;
+    vactivelines >>= lineshifter;
 
     int validh = (hres > 0 && h_start < PRESCALE_WIDTH);
 
