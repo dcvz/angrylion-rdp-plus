9503b4ae701d27a4971ed32cf359b17c0cad7a76
ata4 <barracuda415@yahoo.de>
2017-09-07 15:03:14 +0200

    Moved common OpenGL code of the two plugins to gl_screen

--- plugin-mupen64plus/screen_opengl_m64p.c
+++ plugin-mupen64plus/screen_opengl_m64p.c
@@ -1,8 +1,8 @@
 #include "screen_opengl_m64p.h"
 #include "plugin.h"
-#include "gl_core_3_3.h"
 
 #include "core/msg.h"
+#include "gl-screen/gl_screen.h"
 
 #include <stdlib.h>
 
@@ -22,104 +22,12 @@ ptr_VidExt_GL_SetAttribute       CoreVideo_GL_SetAttribute = NULL;
 ptr_VidExt_GL_GetAttribute       CoreVideo_GL_GetAttribute = NULL;
 ptr_VidExt_GL_SwapBuffers        CoreVideo_GL_SwapBuffers = NULL;
 
-// OpenGL objects
-static GLuint program;
-static GLuint vao;
-static GLuint texture;
-
 static int32_t toggle_fs;
 
 // framebuffer texture states
-static int32_t tex_width;
-static int32_t tex_height;
-static int32_t tex_display_width;
-static int32_t tex_display_height;
 int32_t window_width;
 int32_t window_height;
 int32_t window_fullscreen;
-
-#define TEX_INTERNAL_FORMAT GL_RGBA8
-#define TEX_FORMAT GL_BGRA
-#define TEX_TYPE GL_UNSIGNED_INT_8_8_8_8_REV
-
-// OpenGL helpers
-static GLuint gl_shader_compile(GLenum type, const GLchar* source)
-{
-    GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, NULL);
-    glCompileShader(shader);
-
-    GLint param;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &param);
-
-    if (!param) {
-        GLchar log[4096];
-        glGetShaderInfoLog(shader, sizeof(log), NULL, log);
-        msg_error("%s shader error: %s\n", type == GL_FRAGMENT_SHADER ? "Frag" : "Vert", log);
-    }
-
-    return shader;
-}
-
-static GLuint gl_shader_link(GLuint vert, GLuint frag)
-{
-    GLuint program = glCreateProgram();
-    glAttachShader(program, vert);
-    glAttachShader(program, frag);
-    glLinkProgram(program);
-
-    GLint param;
-    glGetProgramiv(program, GL_LINK_STATUS, &param);
-
-    if (!param) {
-        GLchar log[4096];
-        glGetProgramInfoLog(program, sizeof(log), NULL, log);
-        msg_error("Shader link error: %s\n", log);
-    }
-
-    glDeleteShader(frag);
-    glDeleteShader(vert);
-
-    return program;
-}
-
-static void glSetup(void)
-{
-    // shader sources for drawing a clipped full-screen triangle. the geometry
-    // is defined by the vertex ID, so a VBO is not required.
-    const GLchar* vert_shader =
-        "#version 330 core\n"
-        "out vec2 uv;\n"
-        "void main(void) {\n"
-        "    uv = vec2((gl_VertexID << 1) & 2, gl_VertexID & 2);\n"
-        "    gl_Position = vec4(uv * vec2(2.0, -2.0) + vec2(-1.0, 1.0), 0.0, 1.0);\n"
-        "}\n";
-
-    const GLchar* frag_shader =
-        "#version 330 core\n"
-        "in vec2 uv;\n"
-        "layout(location = 0) out vec4 color;\n"
-        "uniform sampler2D tex0;\n"
-        "void main(void) {\n"
-        "    color = texture(tex0, uv);\n"
-        "}\n";
-
-    // compile and link OpenGL program
-    GLuint vert = gl_shader_compile(GL_VERTEX_SHADER, vert_shader);
-    GLuint frag = gl_shader_compile(GL_FRAGMENT_SHADER, frag_shader);
-    program = gl_shader_link(vert, frag);
-    glUseProgram(program);
-
-    // prepare dummy VAO
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    // prepare texture
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-}
 
 static void screen_init(void)
 {
@@ -144,7 +52,7 @@ static void screen_init(void)
 
     CoreVideo_SetVideoMode(window_width, window_height, 0, window_fullscreen ? M64VIDEO_FULLSCREEN : M64VIDEO_WINDOWED, M64VIDEOFLAG_SUPPORT_RESIZING);
 
-    glSetup();
+    gl_screen_init();
 }
 
 static void screen_swap(void)
@@ -155,59 +63,15 @@ static void screen_swap(void)
         toggle_fs = 0;
     }
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    gl_screen_render(window_width, window_height, 0, 0);
 
-    int32_t vp_width = window_width;
-    int32_t vp_height = window_height;
-
-    // default to bottom left corner of the window above the status bar
-    int32_t vp_x = 0;
-    int32_t vp_y = 0;
-
-    int32_t hw = tex_display_height * vp_width;
-    int32_t wh = tex_display_width * vp_height;
-
-    // add letterboxes or pillarboxes if the window has a different aspect ratio
-    // than the current display mode
-    if (hw > wh) {
-        int32_t w_max = wh / tex_display_height;
-        vp_x += (vp_width - w_max) / 2;
-        vp_width = w_max;
-    } else if (hw < wh) {
-        int32_t h_max = hw / tex_display_width;
-        vp_y += (vp_height - h_max) / 2;
-        vp_height = h_max;
-    }
-
-    // configure viewport
-    glViewport(vp_x, vp_y, vp_width, vp_height);
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
     (*renderCallback)(1);
     CoreVideo_GL_SwapBuffers();
 }
 
 static void screen_upload(int* buffer, int width, int height, int output_width, int output_height)
 {
-    // check if the framebuffer size has changed
-    if (tex_width != width || tex_height != height) {
-        tex_width = width;
-        tex_height = height;
-
-        // reallocate texture buffer on GPU
-        glTexImage2D(GL_TEXTURE_2D, 0, TEX_INTERNAL_FORMAT, tex_width,
-            tex_height, 0, TEX_FORMAT, TEX_TYPE, buffer);
-
-        // update output size
-        tex_display_width = output_width;
-        tex_display_height = output_height;
-
-        msg_debug("screen: resized framebuffer texture: %dx%d", tex_width, tex_height);
-    } else {
-        // copy local buffer to GPU texture buffer
-        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, tex_width, tex_height,
-            TEX_FORMAT, TEX_TYPE, buffer);
-    }
+    gl_screen_upload(buffer, width, height, output_width, output_height);
 }
 
 static void screen_set_fullscreen(bool _fullscreen)
@@ -222,12 +86,7 @@ static bool screen_get_fullscreen(void)
 
 static void screen_close(void)
 {
-    tex_width = 0;
-    tex_height = 0;
-
-    glDeleteTextures(1, &texture);
-    glDeleteVertexArrays(1, &vao);
-    glDeleteProgram(program);
+    gl_screen_close();
 
     CoreVideo_Quit();
 }
--- plugin-zilmar/screen_opengl.c
+++ plugin-zilmar/screen_opengl.c
@@ -1,6 +1,6 @@
 #include "screen_opengl.h"
 #include "gfx_1.3.h"
-#include "gl_core_3_3.h"
+#include "gl-screen/gl_screen.h"
 #include "wgl_ext.h"
 
 #include "core/msg.h"
@@ -9,30 +9,11 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#pragma comment (lib, "opengl32.lib")
-
 extern GFX_INFO gfx;
 
 // default size of the window
 #define WINDOW_DEFAULT_WIDTH 640
 #define WINDOW_DEFAULT_HEIGHT 480
-
-// supposedly, these settings are most hardware-friendly on all platforms
-#define TEX_INTERNAL_FORMAT GL_RGBA8
-#define TEX_FORMAT GL_BGRA
-#define TEX_TYPE GL_UNSIGNED_INT_8_8_8_8_REV
-#define TEX_BYTES_PER_PIXEL sizeof(uint32_t)
-
-// OpenGL objects
-static GLuint program;
-static GLuint vao;
-static GLuint texture;
-
-// framebuffer texture states
-static int32_t tex_width;
-static int32_t tex_height;
-static int32_t tex_display_width;
-static int32_t tex_display_height;
 
 // context states
 static HDC dc;
@@ -54,75 +35,6 @@ void win32_client_resize(HWND hWnd, int nWidth, int nHeight)
     pdiff.y = (rwin.bottom - rwin.top) - rclient.bottom;
 
     MoveWindow(hWnd, rwin.left, rwin.top, nWidth + pdiff.x, nHeight + pdiff.y, TRUE);
-}
-
-// OpenGL helpers
-static GLuint gl_shader_compile(GLenum type, const GLchar* source)
-{
-    GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, NULL);
-    glCompileShader(shader);
-
-    GLint param;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &param);
-
-    if (!param) {
-        GLchar log[4096];
-        glGetShaderInfoLog(shader, sizeof(log), NULL, log);
-        msg_error("%s shader error: %s\n", type == GL_FRAGMENT_SHADER ? "Frag" : "Vert", log);
-    }
-
-    return shader;
-}
-
-static GLuint gl_shader_link(GLuint vert, GLuint frag)
-{
-    GLuint program = glCreateProgram();
-    glAttachShader(program, vert);
-    glAttachShader(program, frag);
-    glLinkProgram(program);
-
-    GLint param;
-    glGetProgramiv(program, GL_LINK_STATUS, &param);
-
-    if (!param) {
-        GLchar log[4096];
-        glGetProgramInfoLog(program, sizeof(log), NULL, log);
-        msg_error("Shader link error: %s\n", log);
-    }
-
-    glDeleteShader(frag);
-    glDeleteShader(vert);
-
-    return program;
-}
-
-static void gl_check_errors(void)
-{
-    GLenum err;
-    while ((err = glGetError()) != GL_NO_ERROR) {
-        char* err_str;
-        switch (err) {
-            case GL_INVALID_OPERATION:
-                err_str = "INVALID_OPERATION";
-                break;
-            case GL_INVALID_ENUM:
-                err_str = "INVALID_ENUM";
-                break;
-            case GL_INVALID_VALUE:
-                err_str = "INVALID_VALUE";
-                break;
-            case GL_OUT_OF_MEMORY:
-                err_str = "OUT_OF_MEMORY";
-                break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION:
-                err_str = "INVALID_FRAMEBUFFER_OPERATION";
-                break;
-            default:
-                err_str = "unknown";
-        }
-        msg_debug("OpenGL error: %d (%s)", err, err_str);
-    }
 }
 
 static void screen_update_size(int32_t width, int32_t height)
@@ -207,67 +119,14 @@ static void screen_init(void)
         msg_warning("Can't create OpenGL 3.3 core context.");
     }
 
-    // shader sources for drawing a clipped full-screen triangle. the geometry
-    // is defined by the vertex ID, so a VBO is not required.
-    const GLchar* vert_shader =
-        "#version 330 core\n"
-        "out vec2 uv;\n"
-        "void main(void) {\n"
-        "    uv = vec2((gl_VertexID << 1) & 2, gl_VertexID & 2);\n"
-        "    gl_Position = vec4(uv * vec2(2.0, -2.0) + vec2(-1.0, 1.0), 0.0, 1.0);\n"
-        "}\n";
-
-    const GLchar* frag_shader =
-        "#version 330 core\n"
-        "in vec2 uv;\n"
-        "layout(location = 0) out vec4 color;\n"
-        "uniform sampler2D tex0;\n"
-        "void main(void) {\n"
-        "    color = texture(tex0, uv);\n"
-        "}\n";
-
-    // compile and link OpenGL program
-    GLuint vert = gl_shader_compile(GL_VERTEX_SHADER, vert_shader);
-    GLuint frag = gl_shader_compile(GL_FRAGMENT_SHADER, frag_shader);
-    program = gl_shader_link(vert, frag);
-    glUseProgram(program);
-
-    // prepare dummy VAO
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    // prepare texture
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    // check if there was an error when using any of the commands above
-    gl_check_errors();
+    gl_screen_init();
 }
 
 static void screen_upload(int* buffer, int width, int height, int output_width, int output_height)
 {
     // check if the framebuffer size has changed
-    if (tex_width != width || tex_height != height) {
-        tex_width = width;
-        tex_height = height;
-
-        // reallocate texture buffer on GPU
-        glTexImage2D(GL_TEXTURE_2D, 0, TEX_INTERNAL_FORMAT, tex_width,
-            tex_height, 0, TEX_FORMAT, TEX_TYPE, buffer);
-
-        // update output size
-        tex_display_width = output_width;
-        tex_display_height = output_height;
-
-        screen_update_size(tex_display_width, tex_display_height);
-
-        msg_debug("screen: resized framebuffer texture: %dx%d", tex_width, tex_height);
-    } else {
-        // copy local buffer to GPU texture buffer
-        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, tex_width, tex_height,
-            TEX_FORMAT, TEX_TYPE, buffer);
+    if (gl_screen_upload(buffer, width, height, output_width, output_height)) {
+        screen_update_size(output_width, output_height);
     }
 }
 
@@ -293,39 +152,17 @@ static void screen_swap(void)
         rect.bottom -= statusrect.bottom;
     }
 
-    int32_t vp_width = rect.right - rect.left;
-    int32_t vp_height = rect.bottom - rect.top;
+    int32_t win_width = rect.right - rect.left;
+    int32_t win_height = rect.bottom - rect.top;
 
     // default to bottom left corner of the window above the status bar
-    int32_t vp_x = 0;
-    int32_t vp_y = statusrect.bottom;
+    int32_t win_x = 0;
+    int32_t win_y = statusrect.bottom;
 
-    int32_t hw = tex_display_height * vp_width;
-    int32_t wh = tex_display_width * vp_height;
-
-    // add letterboxes or pillarboxes if the window has a different aspect ratio
-    // than the current display mode
-    if (hw > wh) {
-        int32_t w_max = wh / tex_display_height;
-        vp_x += (vp_width - w_max) / 2;
-        vp_width = w_max;
-    } else if (hw < wh) {
-        int32_t h_max = hw / tex_display_width;
-        vp_y += (vp_height - h_max) / 2;
-        vp_height = h_max;
-    }
-
-    // configure viewport
-    glViewport(vp_x, vp_y, vp_width, vp_height);
-
-    // draw fullscreen triangle
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    gl_screen_render(win_width, win_height, win_x, win_y);
 
     // swap front and back buffers
     SwapBuffers(dc);
-
-    // check if there was an error when using any of the commands above
-    gl_check_errors();
 }
 
 static void screen_set_fullscreen(bool _fullscreen)
@@ -395,12 +232,7 @@ static bool screen_get_fullscreen(void)
 
 static void screen_close(void)
 {
-    tex_width = 0;
-    tex_height = 0;
-
-    glDeleteTextures(1, &texture);
-    glDeleteVertexArrays(1, &vao);
-    glDeleteProgram(program);
+    gl_screen_close();
 
     if (glrc_core) {
         wglDeleteContext(glrc_core);
