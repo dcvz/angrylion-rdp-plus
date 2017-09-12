8f5956e866c851487df64152d82104901b514d48
Logan McNaughton <logan@bacoosta.com>
2017-09-12 12:11:05 -0600

    Fix vprintf in msg.c
    buf is only used for vsprintf_s

--- core/msg.c
+++ core/msg.c
@@ -12,14 +12,14 @@
 
 void msg_error(const char * err, ...)
 {
-    char buf[MSG_BUFFER_LEN];
     va_list arg;
     va_start(arg, err);
 #ifdef WIN32
+    char buf[MSG_BUFFER_LEN];
     vsprintf_s(buf, sizeof(buf), err, arg);
     MessageBoxA(0, buf, "RDP: fatal error", MB_OK);
 #else
-    vprintf(buf, arg);
+    vprintf(err, arg);
 #endif
     va_end(arg);
     exit(0);
@@ -27,28 +27,28 @@ void msg_error(const char * err, ...)
 
 void msg_warning(const char* err, ...)
 {
-    char buf[MSG_BUFFER_LEN];
     va_list arg;
     va_start(arg, err);
 #ifdef WIN32
+    char buf[MSG_BUFFER_LEN];
     vsprintf_s(buf, sizeof(buf), err, arg);
     MessageBoxA(0, buf, "RDP: warning", MB_OK);
 #else
-    vprintf(buf, arg);
+    vprintf(err, arg);
 #endif
     va_end(arg);
 }
 
 void msg_debug(const char* err, ...)
 {
-    char buf[MSG_BUFFER_LEN];
     va_list arg;
     va_start(arg, err);
 #ifdef WIN32
+    char buf[MSG_BUFFER_LEN];
     vsprintf_s(buf, sizeof(buf), err, arg);
     OutputDebugStringA(buf);
 #else
-    vprintf(buf, arg);
+    vprintf(err, arg);
 #endif
     va_end(arg);
 }
