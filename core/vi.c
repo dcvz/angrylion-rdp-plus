9b0a93486e09eb3fd0a7b5c081c07045716e177a
ata4 <barracuda415@yahoo.de>
2017-09-28 13:04:58 +0200

    Added a few comments

--- core/vi.c
+++ core/vi.c
@@ -1477,6 +1477,8 @@ static void vi_process_end(void)
 
 static int vi_process_start_fast(void)
 {
+    // note: this is probably a very, very crude method to get the frame size,
+    // but should hopefully work most of the time
     int32_t v_start = (*vi_reg_ptr[VI_V_START] >> 16) & 0x3ff;
     int32_t h_start = (*vi_reg_ptr[VI_H_START] >> 16) & 0x3ff;
 
@@ -1496,7 +1498,15 @@ static int vi_process_start_fast(void)
     hres_raw = x_add * hres / 1024;
     vres_raw = y_add * vres / 1024;
 
-    if (hres_raw <= 0 || vres_raw <= 0 || *vi_reg_ptr[VI_V_CURRENT_LINE] & 1) {
+    if (hres_raw <= 0 || vres_raw <= 0) {
+        return 0;
+    }
+
+    // drop every other interlaced frame to avoid "wobbly" output due to the
+    // vertical offset
+    // TODO: completely skip rendering these frames in unfiltered to improve
+    // performance?
+    if (*vi_reg_ptr[VI_V_CURRENT_LINE] & 1) {
         return 0;
     }
 
