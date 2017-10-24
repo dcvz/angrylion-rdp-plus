5c28fa9a12f10e848ff95f796e419e5d50930b4d
ata4 <barracuda415@yahoo.de>
2017-10-24 22:25:11 +0200

    Re-fixed #19
    Fixes #32

--- core/vi.c
+++ core/vi.c
@@ -605,12 +605,17 @@ static void vi_process_end(void)
 
 static bool vi_process_start_fast(void)
 {
+    // skip invalid/unsupported frame sizes
+    if (hres <= 0 || vres <= 0) {
+        return 0;
+    }
+
     // note: this is probably a very, very crude method to get the frame size,
     // but should hopefully work most of the time
     hres_raw = x_add * hres / 1024;
     vres_raw = y_add * vres / 1024;
 
-    // skip invalid frame sizes
+    // check frame sizes again, just in case
     if (hres_raw <= 0 || vres_raw <= 0) {
         return false;
     }
