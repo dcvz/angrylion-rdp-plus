dcvz/angrylion-rdp-plus

commit d52d94fe1e9e75b8ba64f6e6ab2a88924b5f461f

Fixed a deadlock when tasks are scheduled while the worker threads haven't reached m_signal_work.wait() yet
Fixes #44

core/parallel.cpp
@@ -11,20 +11,23 @@ class Parallel
 {
 public:
     Parallel(uint32_t num_workers) {
+        std::unique_lock<std::mutex> ul(m_mutex);
+
         // create worker threads
         for (uint32_t worker_id = 0; worker_id < num_workers; worker_id++) {
             m_workers.emplace_back(std::thread(&Parallel::do_work, this, worker_id));
         }
+
+        // give workers an empty task and wait for them to finish it to
+        // make sure they're ready
+        m_task = [](uint32_t){};
+        m_workers_active = m_workers.size();
+        m_signal_done.wait(ul);
     }
 
     ~Parallel() {
         // wait for all workers to finish their current work
         wait();
-
-        // set a dummy task in case nothing has been done yet
-        if (!m_task) {
-            m_task = [](uint32_t) {};
-        }
 
         // exit worker main loops
         m_accept_work = false;
@@ -66,12 +69,14 @@ private:
     void do_work(int32_t worker_id) {
         std::unique_lock<std::mutex> ul(m_mutex);
         while (m_accept_work) {
-            m_signal_work.wait(ul);
             ul.unlock();
-            m_task(worker_id);
+            if (m_accept_work) {
+                m_task(worker_id);
+            }
             ul.lock();
             m_workers_active--;
             m_signal_done.notify_all();
+            m_signal_work.wait(ul);
         }
     }
 

commit 4c4a265547ee1f4253ba94476cc4609e7b2807ca

Wait for all workers in constructor, not just one
This should fix several weird glitches, hangs and crashes introduced by d52d94f. May help #44 as well.

core/parallel.cpp
@@ -23,7 +23,9 @@ public:
 
         // wait for workers to finish task to make sure they're ready
         m_workers_active = m_workers.size();
-        m_signal_done.wait(ul);
+        while (m_workers_active) {
+            m_signal_done.wait(ul);
+        }
     }
 
     ~Parallel() {