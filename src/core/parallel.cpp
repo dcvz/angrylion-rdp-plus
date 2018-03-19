8ee86bc4f8e88ebe378d0faecf65216d1a438a69
ata4 <barracuda415@yahoo.de>
2018-03-19 13:29:37 +0100

    Improved synchronization code based on fzurita's suggestions and a few own ideas

--- src/core/parallel.cpp
+++ src/core/parallel.cpp
@@ -1,25 +1,35 @@
 #include "parallel.h"
 
-#include <functional>
-#include <vector>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
 #include <atomic>
+#include <algorithm>
+#include <condition_variable>
+#include <cstdint>
+#include <functional>
+#include <mutex>
+#include <thread>
+#include <vector>
 
 class Parallel
 {
 public:
-    Parallel(uint32_t num_workers) : m_workers_active(num_workers) {
+    Parallel(std::uint32_t num_workers) :
+        m_num_workers(std::min(num_workers, 64U))
+    {
+        // mask for m_tasks_done when all workers have finished their task
+        // except for worker 0, which runs in the main thread
+        m_all_tasks_done = ((1Ui64 << m_num_workers) - 1) & ~1;
+
         // give workers an empty task
-        m_task = [](uint32_t) {};
+        m_task = [](std::uint32_t) {};
+        m_accept_work = true;
+        start_work();
 
         // create worker threads
-        for (uint32_t worker_id = 0; worker_id < num_workers; worker_id++) {
+        for (std::uint32_t worker_id = 1; worker_id < m_num_workers; worker_id++) {
             m_workers.emplace_back(std::thread(&Parallel::do_work, this, worker_id));
         }
 
-        // wait for workers to finish task to make sure they're ready
+        // synchronize workers to prepare them for real tasks
         wait();
     }
 
@@ -29,7 +39,7 @@ public:
 
         // exit worker main loops
         m_accept_work = false;
-        m_signal_work.notify_all();
+        start_work();
 
         // join worker threads to make sure they have finished
         for (auto& thread : m_workers) {
@@ -40,7 +50,7 @@ public:
         m_workers.clear();
     }
 
-    void run(std::function<void(uint32_t)>&& task) {
+    void run(std::function<void(std::uint32_t)>&& task) {
         // don't allow more tasks if workers are stopping
         if (!m_accept_work) {
             throw std::runtime_error("Workers are exiting and no longer accept work");
@@ -48,48 +58,70 @@ public:
 
         // prepare task for workers and send signal so they start working
         m_task = task;
-        m_workers_active = m_workers.size();
-        m_signal_work.notify_all();
+        start_work();
+
+        // run worker 0 directly on main thread
+        m_task(0);
 
         // wait for all workers to finish
         wait();
     }
 
+    std::uint32_t num_workers() {
+        return m_num_workers;
+    }
+
 private:
-    std::function<void(uint32_t)> m_task;
+    std::function<void(std::uint32_t)> m_task;
     std::vector<std::thread> m_workers;
-    std::mutex m_mutex;
+    std::mutex m_signal_mutex;
     std::condition_variable m_signal_work;
     std::condition_variable m_signal_done;
-    std::atomic_size_t m_workers_active;
-    std::atomic_bool m_accept_work{true};
+    std::atomic_uint64_t m_tasks_done;
+    std::uint64_t m_all_tasks_done;
+    std::atomic_bool m_accept_work;
+    const std::uint32_t m_num_workers;
 
-    void do_work(int32_t worker_id) {
-        std::unique_lock<std::mutex> ul(m_mutex);
+    void start_work() {
+        std::unique_lock<std::mutex> ul(m_signal_mutex);
+
+        // clear task bits for all workers
+        m_tasks_done = 0;
+
+        // wake up all workers
+        m_signal_work.notify_all();
+    }
+
+    void do_work(std::uint32_t worker_id) {
+        const std::uint64_t worker_mask = 1Ui64 << worker_id;
+
         while (m_accept_work) {
-            // switch to async mode and run task
-            ul.unlock();
-            if (m_accept_work) {
-                m_task(worker_id);
+            // do the work
+            m_task(worker_id);
+
+            {
+                std::unique_lock<std::mutex> ul(m_signal_mutex);
+
+                // mark task as done
+                m_tasks_done |= worker_mask;
+
+                // notify main thread
+                m_signal_done.notify_one();
+
+                // take a break and wait for more work
+                m_signal_work.wait(ul, [worker_mask, this] {
+                    return (m_tasks_done & worker_mask) == 0;
+                });
             }
-            ul.lock();
-
-            // task is done, update number of active workers
-            // and signal main thread
-            m_workers_active--;
-            m_signal_done.notify_all();
-
-            // go idle and wait for more work
-            m_signal_work.wait(ul);
         }
     }
 
     void wait() {
-        std::unique_lock<std::mutex> ul(m_mutex);
-        while (m_workers_active) {
-            std::this_thread::yield();
-            m_signal_done.wait(ul);
-        }
+        // wait for all workers to set their task bits
+        std::unique_lock<std::mutex> ul(m_signal_mutex);
+        m_signal_done.wait(ul, [this] {
+            return m_tasks_done == m_all_tasks_done;
+        });
     }
 
     void operator=(const Parallel&) = delete;
@@ -98,7 +130,6 @@ private:
 
 // C interface for the Parallel class
 static std::unique_ptr<Parallel> parallel;
-static uint32_t worker_num;
 
 void parallel_init(uint32_t num)
 {
@@ -108,8 +139,6 @@ void parallel_init(uint32_t num)
     }
 
     parallel = std::make_unique<Parallel>(num);
-
-    worker_num = num;
 }
 
 void parallel_run(void task(uint32_t))
@@ -119,7 +148,7 @@ void parallel_run(void task(uint32_t))
 
 uint32_t parallel_worker_num()
 {
-    return worker_num;
+    return parallel->num_workers();
 }
 
 void parallel_close()
