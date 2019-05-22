bd928eb5718870a88f476c3542a5667d049e1843
ata4 <barracuda415@yahoo.de>
2019-05-22 09:56:02 +0200

    Fixed wrong variable name for screen_read definition

--- src/core/screen.h
+++ src/core/screen.h
@@ -24,7 +24,7 @@ struct frame_buffer
 void screen_init(struct n64video_config* config);
 void screen_swap(bool blank);
 void screen_write(struct frame_buffer* fb, int32_t output_height);
-void screen_read(struct frame_buffer* fb, bool rgb);
+void screen_read(struct frame_buffer* fb, bool alpha);
 void screen_set_fullscreen(bool fullscreen);
 bool screen_get_fullscreen(void);
 void screen_toggle_fullscreen(void);
